350859268f26d49f8a57be4c3889cdce6f00ff97
riniwtz <riniwtzcode@gmail.com>
2021-10-30 22:56:46 +0800

    Update

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.JDABuilder;
 public class Bot {
 
     public static void main(String[] args) throws Exception {
-        JDA jda = JDABuilder.createDefault("OTAzOTMyMDkzODYyNjU4MDU5.YX0KXQ.IY__8zbOwweAtjV4zZvDoAy72OQ").build();
+        JDA jda = JDABuilder.createDefault("OTAzOTMyMDkzODYyNjU4MDU5.YX0KXQ.mDSMDh8q9MC5iVAPZMY1qdT_mwE").build();
         jda.addEventListener(new TestEvent());
     }
 }
