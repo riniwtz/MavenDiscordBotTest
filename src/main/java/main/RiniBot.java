fbbf2b1e6e5c6595ccaa35848d2560512951c722
riniwtz <riniwtzcode@gmail.com>
2021-11-02 00:41:15 +0800

    Fix

--- src/main/java/main/RiniBot.java
+++ src/main/java/main/RiniBot.java
@@ -1,15 +1,14 @@
 package main;
 
-import events.CommandList;
-import events.MessageFilter;
+import events.BotCommandList;
+import events.BotMessageFilter;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 public class RiniBot {
-    public static char prefix = '-';
     public static void main(String[] args) throws Exception {
         JDA jda = JDABuilder.createDefault("").build();
-        jda.addEventListener(new CommandList());
-        jda.addEventListener(new MessageFilter());
+        jda.addEventListener(new BotCommandList());
+        jda.addEventListener(new BotMessageFilter());
     }
 }
