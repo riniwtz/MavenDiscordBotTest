8db3f7588d76ff660f1f55c88654a0a2d0e3a4d6
riniwtz <riniwtzcode@gmail.com>
2021-11-03 14:23:22 +0800

    Update

--- src/main/java/main/RiniBot.java
+++ src/main/java/main/RiniBot.java
@@ -1,14 +1,23 @@
 package main;
 
-import events.BotCommandList;
-import events.BotMessageFilter;
+import events.*;
+import events.BotBananaFilter;
+import logger.Logger;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 public class RiniBot {
+
     public static void main(String[] args) throws Exception {
         JDA jda = JDABuilder.createDefault("").build();
+        // events
+        jda.addEventListener(new BaseCommand());
         jda.addEventListener(new BotCommandList());
         jda.addEventListener(new BotMessageFilter());
+        jda.addEventListener(new BotPrefix());
+        // logger
+        jda.addEventListener(new Logger());
+        // banana
+        jda.addEventListener(new BotBananaFilter());
     }
 }
