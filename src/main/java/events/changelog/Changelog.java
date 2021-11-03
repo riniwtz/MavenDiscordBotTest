44a73a2cec4073e9e63c85bfabed9b35de593e37
riniwtz <riniwtzcode@gmail.com>
2021-11-03 15:55:52 +0800

    Add new command

---
+++ src/main/java/events/changelog/Changelog.java
@@ -0,0 +1,18 @@
+package events.changelog;
+
+import events.BotPrefix;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import static events.BaseCommand.*;
+
+public class Changelog extends ListenerAdapter {
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+        if (command.equals(BotPrefix.prefix + "changelog")) {
+            sendMessage(event, "Changelog Update v1.0 Beta:\n" +
+                    "+ Added new commands (-commands, -prefix, -filter)" +
+                    "+ Added Logger", false);
+        }
+    }
+}
