8e62f95db681f167ac901d0ee496536a0b8b2457
riniwtz <riniwtzcode@gmail.com>
2021-11-04 14:06:08 +0800

    Update

--- src/main/java/main/RiniBot.java
+++ src/main/java/main/RiniBot.java
@@ -2,6 +2,7 @@ package main;
 
 import events.*;
 import events.banana.BotBananaFilter;
+import events.changelog.Changelog;
 import logger.Logger;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -19,5 +20,28 @@ public class RiniBot {
         jda.addEventListener(new Logger());
         // banana
         jda.addEventListener(new BotBananaFilter());
+        // changelog
+        jda.addEventListener(new Changelog());
+
+        // Done:
+        /*
+         * + Logger
+         * + Changelog
+         * + Fixed messages
+         * + Finish BotMessageFilter class development
+         * + Finish BotPrefix class development
+         * + Finish BaseCommand class development
+         * + Finish BotCommandList class development
+         * + Added Banana Commands
+         */
+
+        // TODO:
+        /*
+         * + Microsoft document modifier
+         * + Contribute command
+         * + Prevent admins and mods from being affected by word filter
+         * + Fix update message event to prevent bypassers
+         * + Learn role and server name or server exclusion method in JDA
+         */
     }
 }
